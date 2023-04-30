1948c5b7633262ca6322f17452ffff3061f6f754
Petar Dobrev <peterdobrevxd@gmail.com>
2023-04-30 11:16:29 +0300

    Initial

---
+++ CoolEvents/CoolEvents/Startup.cs
@@ -0,0 +1,57 @@
+﻿using CoolEvents.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolEvents
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CoolEventsDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("CoolEventsDB")));
+
+            services.AddControllersWithViews();
+
+            services.AddSession();
+            services.AddHttpContextAccessor();
+
+            // Other service registrations
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseSession();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ CoolEventsDB-Create.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE [CoolEventsDB];
+GO;
+
+USE [CoolEventsDB];
+
+
+-- Create Role table
+CREATE TABLE [Role] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL UNIQUE
+);
+
+-- Create User table
+CREATE TABLE [User] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [Username] NVARCHAR(50) NOT NULL UNIQUE,
+    [Password] NVARCHAR(100) NOT NULL,
+    [FirstName] NVARCHAR(50) NOT NULL,
+    [LastName] NVARCHAR(50) NOT NULL,
+    [RoleId] INT NOT NULL,
+    CONSTRAINT [FK_User_Role] FOREIGN KEY ([RoleId]) REFERENCES [Role]([Id])
+);
+
+-- Create Event table
+CREATE TABLE [Event] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(64) NOT NULL,
+    [Description] NVARCHAR(255) NOT NULL,
+    [Image] VARBINARY(MAX) NOT NULL,
+    [Date] DATE NOT NULL
+);
+
+-- Create Ticket table
+CREATE TABLE [Ticket] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [UserId] INT NOT NULL,
+    [EventId] INT NOT NULL,
+    CONSTRAINT [FK_Ticket_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
+    CONSTRAINT [FK_Ticket_Event] FOREIGN KEY ([EventId]) REFERENCES [Event]([Id])
+);
+
+
+-- Add default roles to Role table
+INSERT INTO [Role] ([Name])
+VALUES ('Admin'), ('User');
